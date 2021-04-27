a3db08eaab77e450c3d314ba6d89c82b198d86ea
Christian Dionisio <christiandionisio9432@gmail.com>
2021-04-26 22:44:13 -0500

    Estructura del proyecto + Servicio getPosts

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../../services/posts.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+
+  constructor(private postService: PostsService) {}
+
+  ngOnInit() {
+    this.postService.getPosts()
+      .subscribe( resp => {
+        console.log(resp);
+      });
+
+  }
+
+}
